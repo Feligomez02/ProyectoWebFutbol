754cbf5437f5eec0b47f51332237b235412b0ffd
Felipe Gomez <94563@sistemas.frc.utn.edu.ar>
2024-05-24 17:45:27 -0300

    Agregue al modelo tabla de paises y ciudades

--- base-orm/sqlite-init.js
+++ base-orm/sqlite-init.js
@@ -3,7 +3,7 @@ const db = require("aa-sqlite");
 
 async function CrearBaseSiNoExiste() {
   // abrir base, si no existe el archivo/base lo crea
-  await db.open("./.data/pymes.db");
+  await db.open("./.data/basetp2.db");
   //await db.open(process.env.base);
 
   let existe = false;
@@ -26,51 +26,50 @@ async function CrearBaseSiNoExiste() {
 
   existe = false;
   res = await db.get(
-    "SELECT count(*) as contar FROM sqlite_schema WHERE type = 'table' and name= 'articulosfamilias'",
+    "SELECT count(*) as contar FROM sqlite_schema WHERE type = 'table' and name= 'paises'",
     []
   );
   if (res.contar > 0) existe = true;
   if (!existe) {
     await db.run(
-      "CREATE table articulosfamilias( IdArticuloFamilia INTEGER PRIMARY KEY AUTOINCREMENT, Nombre text NOT NULL UNIQUE);"
+      "CREATE table paises( IdPais INTEGER PRIMARY KEY AUTOINCREMENT, Nombre text NOT NULL UNIQUE, Fecha date);"
     );
-    console.log("tabla articulosfamilias creada!");
+    console.log("tabla de paises creada!");
     await db.run(
-      "insert into articulosfamilias values	(1,'ACCESORIOS'),(2,'AUDIO'),(3,'CELULARES'),(4,'CUIDADO PERSONAL'),(5,'DVD'),(6,'FOTOGRAFIA'),(7,'FRIO-CALOR'),(8,'GPS'),(9,'INFORMATICA'),(10,'LED - LCD');"
+      "insert into paises values    (1,'Argentina','2022-01-01'),(2,'Brasil', '2022-01-02'),(3,'Alemania', '2022-01-03'),(4,'USA', '2022-01-04'),(5,'Francia, '2022-01-05'),(6,'Canada', '2022-01-06'),(7,'Spain', '2022-01-07'),(8,'Chile', '2022-01-08'),(9,'Italia', '2022-01-09'),(10,'Inglaterra', '2022-01-10');"
     );
   }
 
   existe = false;
-  sql =
-    "SELECT count(*) as contar FROM sqlite_schema WHERE type = 'table' and name= 'articulos'";
-  res = await db.get(sql, []);
+  tablaCiudades =
+    "SELECT count(*) as contar FROM sqlite_schema WHERE type = 'table' and name= 'ciudades'";
+  res = await db.get(tablaCiudades, []);
   if (res.contar > 0) existe = true;
   if (!existe) {
     await db.run(
-      `CREATE table articulos( 
-              IdArticulo INTEGER PRIMARY KEY AUTOINCREMENT
+      `CREATE table ciudades( 
+              IdCiudad INTEGER PRIMARY KEY AUTOINCREMENT
             , Nombre text NOT NULL UNIQUE
-            , Precio real
-            , CodigoDeBarra
-            , IdArticuloFamilia integer
-            , Stock integer
-            , FechaAlta text
-            , Activo boolean,
-            FOREIGN KEY (IdArticuloFamilia) REFERENCES articulosfamilias(IdArticuloFamilia)
+            , FechaCiudad date
+            , IdPais INTEGER NOT NULL
+            ,FOREIGN KEY (IdPais) REFERENCES ciudades(IdPais)
             );`
     );
-    console.log("tabla articulos creada!");
+    console.log("tabla de ciudades creada!");
 
     await db.run(
-      `insert into articulos values
-      (1,'KIT DIRECT TV PREPA 0.60MT',299.00, '0779815559001', 10, 329,'2017-01-19', 1 ),
-      (2,'KIT DIRECT TV PREPA 0.90MT',349.00, '0779815559002', 10, 468,'2017-01-31', 1 ),
-      (3,'LED 22" LG FHD 22MN42APM',2669.00, '0779808338808', 10, 536,'2017-01-12', 1 ),
-      (199,'CAMARA DIGITAL C1433 SLVER GE',899.00, '0084695100018', 6, 528,'2017-02-02', 1 ),
-      (200,'LIMPIADOR CD SV 8336 ONE FOR ALL',55.00, '0871618404342', 1, 508,'2016-12-27', 1 ),
-      (201,'LIMPIADOR LCD SV 8410 ONE FOR ALL',102.00, '0871618404333', 1, 186,'2017-02-02', 1 )
-      ;`
-    );
+    `INSERT INTO ciudades (Nombre, FechaCiudad, IdPais) VALUES 
+    ('Cordoba', '2022-01-01', 1),
+    ('Rio de Janeiro', '2022-01-02', 2),
+    ('Madrid', '2022-01-03', 7),
+    ('Roma', '2022-01-04', 9),
+    ('Londres', '2022-01-05', 10),
+    ('Santiago', '2022-01-06', 8),
+    ('Berlin', '2022-01-07', 3),
+    ('Miami', '2022-01-08', 4),
+    ('Paris', '2022-01-09', 5),
+    ('Vancouver', '2022-01-10', 6);`
+);
   }
 
   // cerrar la base
