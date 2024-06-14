35f7b93957294e9b282e3d963bb410f20a54b7c2
Felipe Gomez <94563@sistemas.frc.utn.edu.ar>
2024-06-14 16:56:51 -0300

    chore: Update page title and footer text

--- Frontend/frontend-futbol/src/components/Inicio.jsx
+++ Frontend/frontend-futbol/src/components/Inicio.jsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 function Inicio() {
   return (
     <div className="mt-4 p-5 rounded" style={{ backgroundColor: "lightgray" }}>
-      <h1>WEB FUTBOL</h1>
-      <p>Este ejemplo está desarrollado con las siguientes tecnologías:</p>
+      <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo_lpf_afa.png" 
+          alt="Logo LPF AFA" 
+          style={{ width: '50px', marginRight: '20px' }} 
+        />
+      <h1>LIGA PROFESIONAL DE FUTBOL</h1>
+      
+      <p>Este TP está desarrollado con las siguientes tecnologías:</p>
       <p>
         Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite
         múltiples capas en Javascript.
